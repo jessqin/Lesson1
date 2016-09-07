863cb7be2707fe2ebce2ba529f743757b685258a
Jessica N Qin <jq210@pvhs-lab112-d15.pvsd.org>
2016-09-07 14:20:45 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello World");
+        System.out.print("What is going on");
+        //Line comment is done with a //
+        /* is a block comment */
+        System.out.println(1);
+    }
+}
